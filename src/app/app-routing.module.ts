a425b090a4f23903652f42a2e2f559f7d607e44b
Omkar <omkardeshmukh162001@gmail.com>
2023-07-21 03:04:11 +0530

    Routing with Example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '' }, // Redirect any other path to the default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
